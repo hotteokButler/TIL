b7a028e670c9eefc59239ce7f9536d3f795519f5
jisooyoon <ygs12567@gmail.com>
2023-01-22 15:54:47 +0900

    Doc : typescript project 기본 셋팅

---
+++ typescript/prac_project/typeChain/build/myPackage.js
@@ -0,0 +1,22 @@
+// javasript를 migration하지 않고 그대로 type protection과 함께 쓰려면
+// tsconfig.json 에서 "compilerOptions" 에 { "allowJS" : true } 후 해당 js module file 에서 "@ts-check" 사용
+// + JSDoc 함께 사용 : function with comments
+//@ts-check
+/**
+ * Initialize the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+export function init(config) {
+    return true;
+}
+/**
+ * Exits the program
+ * @param {number} number
+ * @returns number
+ */
+export function exit(number) {
+    return number + 1;
+}
